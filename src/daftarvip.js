e82fb6928b66623aa43aa8b17ecb6740e25eb81a
GETZIIN <gustavopaula620@gmail.com>
2021-07-21 23:08:25 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/554599174965 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO 𝑭𝑹𝑨𝑵𝑲𝒁𝑰𝑰𝑵 🐊🚩 :*
+_https://chat.whatsapp.com/IrKmqmFSUsUA1NUPS7D52l_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 𝑮𝑬𝑻𝒁𝑰𝑰𝑵 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,PIX*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/554599174965*
+║
+╚═〘  𝑭𝑹𝑨𝑵𝑲𝒁𝑰𝑰𝑵  〙
+`
+}
+exports.iklan = iklan
